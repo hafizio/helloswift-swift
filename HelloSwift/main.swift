6dcafc3cc61fb60643e4014b1251f547971050e8
Hafiz Khairudin <ibnukamy@gmail.com>
2015-08-09 21:12:34 +0800

    Initial Commit

---
+++ HelloSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloSwift
+//
+//  Created by Hafiz Khairudin on 8/9/15.
+//  Copyright © 2015 Hafiz Khairudin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
